e9d0a00d1257803e06eb907d1ee39c7588d37d32
Harsh Patel <Patelharsh241999@gmail.com>
2023-09-27 22:33:22 +0530

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import { memo } from "preact/compat"
+
+const App=()=> {
+
+  return (
+    <>
+      <div className="text-2xl m-auto text-center">Hello There</div>
+    </>
+  )
+}
+export default memo(App)
