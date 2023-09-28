00118452992879d124850e55948399023a3d4dfd
Harsh Patel <Patelharsh241999@gmail.com>
2023-09-28 09:47:34 +0530

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -63,7 +63,7 @@ const CountryCapitalGame: FunctionComponent<{
   setShowItems(updatedItems);
 };
   return (
-    <div className="flex flex-wrap gap-5 justify-around align-center">
+    <div className="flex flex-col flex-wrap gap-5 justify-around align-center">
       {showItems.length != 0 ? (
         showItems.map((e, i) => {
           return (
@@ -85,7 +85,7 @@ const CountryCapitalGame: FunctionComponent<{
       )}
       <button
   onClick={handleReset}
-  className="w-32 h-10 bg-blue-500 text-white rounded-full mt-4"
+  className="w-32 h-10 bg-blue-500 text-white mt-4"
 >
   Reset
 </button>
